939f46f59feda4f91c573a1d9ac177a1c0119058
Dimitris <dimitris.strantsalis@workable.com>
2022-01-08 13:19:45 +0200

    ✨  Some more helpers on docker lib

--- lib/docker/main.go
+++ lib/docker/main.go
@@ -1,11 +1,27 @@
-package deploy
+package containers
 
 import (
+	"errors"
+	"fmt"
+	"strings"
+
 	log "github.com/sirupsen/logrus"
 
 	"github.com/docker/docker/api/types/swarm"
 	docker "github.com/fsouza/go-dockerclient"
 )
+
+//Parse image name and tag
+func ParseImageName(full_name string) (string, string, error) {
+	result := strings.Split(full_name, ":")
+
+	if len(result) != 2 {
+		err := errors.New("Parsing failed for image: " + full_name)
+		return "", "", err
+	}
+
+	return result[0], result[1], nil
+}
 
 // Selects the swarm services that need to be updated based on image
 // and swarm label
@@ -24,7 +40,19 @@ func LocateServices(image string) []swarm.Service {
 	var toBeUpdated []swarm.Service
 
 	for _, service := range allServices {
-		if service.Spec.Labels["com.docker.stack.image"] == image && service.Spec.Labels["swarm_deploy"] == "true" {
+		currentImage, _, imageError := ParseImageName(service.Spec.Labels["com.docker.stack.image"])
+
+		if imageError != nil {
+			log.WithFields(
+				log.Fields{
+					"incoming_image": image,
+					"current_image":  service.Spec.Labels["com.docker.stack.image"],
+					"service":        service.PreviousSpec.Name,
+				}).Error("Could not parse current image to compare with incoming. Service will be skipped.")
+			continue
+		}
+
+		if currentImage == image && service.Spec.Labels["swarm_deploy"] == "true" {
 			toBeUpdated = append(toBeUpdated, service)
 		}
 	}
@@ -38,6 +66,8 @@ func UpdateServiceImage(service swarm.Service, image string) bool {
 	if err != nil {
 		panic(err)
 	}
+
+	service.Spec.TaskTemplate.ContainerSpec.Image = image
 
 	var updateConfig docker.UpdateServiceOptions
 	updateConfig.ServiceSpec = service.Spec
@@ -53,14 +83,20 @@ func UpdateServiceImage(service swarm.Service, image string) bool {
 }
 
 // Parent function that triggers locating and updating services
-func UpdateAllServices(image string) {
+func UpdateAllServices(image, tag string) {
 	services := LocateServices(image)
 
+	if len(services) == 0 {
+		log.WithFields(log.Fields{"image": image}).Info("No services found for the given image.")
+	}
+
+	full_image := fmt.Sprintf("%s:%s", image, tag)
+
 	for _, service := range services {
-		result := UpdateServiceImage(service, image)
+		result := UpdateServiceImage(service, full_image)
 
 		if result {
-			log.WithFields(log.Fields{"service": service.Spec.Name, "image": image}).Info("Service image updated")
+			log.WithFields(log.Fields{"service": service.Spec.Name, "image": full_image}).Info("Service image updated")
 		}
 	}
 }
